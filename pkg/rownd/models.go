26b1c9c76644e0430cc73a159066066f74767dc7
rgthelen <rgthelen@gmail.com>
2024-11-19 22:24:17 -0600

    bugs

--- pkg/rownd/models.go
+++ pkg/rownd/models.go
@@ -4,10 +4,10 @@ import "time"
 
 // User represents a Rownd user
 type User struct {
-    ID        string                 `json:"id"`
+    ID        string                 `json:"id,omitempty"`
     Data      map[string]interface{} `json:"data"`
-    CreatedAt time.Time             `json:"created_at"`
-    UpdatedAt time.Time             `json:"updated_at"`
+    CreatedAt time.Time             `json:"created_at,omitempty"`
+    UpdatedAt time.Time             `json:"updated_at,omitempty"`
 }
 
 // TokenValidation represents the response from token validation
